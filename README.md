62a54c14461bb6bf4e6fd71d06b08f38adc2ac9b
Ioana Iosub <stu.waltham@gmail.com>
2024-01-11 15:37:22 +0000

    JSON

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # JSON-API-JQuery
+
+## JSON\_ javaScript Object Notation
+
+Transforms information between a server and a client, also suitable for validating email address and storin user submission form.
+
+JSON.parse()- wol transforme a strong into an object
--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+let companies = `{
+  firstname: "Ioana",
+  secondname: "Iosub",
+  favoriteNumber: 3,
+  "hobbies:": ["rugby", "hiking", "dancing"],
 
+  friends: [
+    {
+      name: "Cristi",
+      age: 28,
+      isProgramer: true,
+    },
+    {
+      name: "Ioana",
+      age: 28,
+      isProgramer: false,
+    },
+  ],
+};`;
+
+console.log(JSON.parse(companies));
